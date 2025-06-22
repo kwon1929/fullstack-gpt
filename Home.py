4d05cc37aef5f345b6629d2244fef0297254cc59
(andrea) <(kwonhyuk0131@gmail.com)>
2025-06-22 20:09:53 +0900

    first document GPT commit

---
+++ Home.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Toss Sales Copliot",
+    page_icon="🤖",
+)
+
+st.markdown(
+    """
+# Hello!
+            
+this is Toss Sales Copliot!
+            
+Here are the apps I made:
+            
+- [ ] [ProposalGPT](/ProposalGPT)
+- [ ] [MeetinglogGPT](/MeetinglogGPT)
+- [ ] [OnboradingGPT](/OnboradingGPT)
+"""
+)
