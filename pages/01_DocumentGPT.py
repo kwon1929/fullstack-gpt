7aec21f1ce3fa5e87603664888d5100ba088fb87
(andrea) <(kwonhyuk0131@gmail.com)>
2025-06-26 22:15:22 +0900

    chat memory

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -11,6 +11,9 @@ st.set_page_config(
     page_icon="📄",
 )
 
+
+
+@st.cache_resource(show_spinner=True)
 def embed_file(file):
     file_content = file.read()
     file_path = f"./.cache/files/{file.name}"
@@ -23,7 +26,7 @@ def embed_file(file):
         chunk_size=600,
         chunk_overlap=100,
     )
-    loader = UnstructuredFileLoader("./files/PhysicalAI.txt")
+    loader = UnstructuredFileLoader(file_path)
     docs = loader.load_and_split(text_splitter=splitter)
     # 4. 임베딩 + 캐시 
     embeddings = OpenAIEmbeddings()
@@ -32,19 +35,44 @@ def embed_file(file):
     vectorstore = FAISS.from_documents(docs, cached_embeddings)
     retriever = vectorstore.as_retriever()
     return retriever
-    
+
+def send_message(message, role, save=True):
+    with st.chat_message(role):
+        st.markdown(message)
+    if save: 
+        st.session_state["messages"].append({"message": message, "role": role})
+
+
+def paint_history():
+    for message in st.session_state["messages"]:
+        send_message(message["message"], message["role"], save=False)
+
+
 st.title("DocumentGPT")
 
 st.markdown("""
 Welcome to DocumentGPT! 
             
 Use this DocumentGPT to generate summaries, insights, and analyses from your documents.
+            
+you can upload a file in sidebar
 """)
 
-file = st.file_uploader("Upload your document", type=["pdf", "docx", "txt"])
+with st.sidebar:
+    file = st.file_uploader("Upload your document", type=["pdf", "docx", "txt"])
 
 if file:
     retriever = embed_file(file)
-    s = retriever.invoke("Summarize the document in 3 sentences.")
+    send_message("I'm ready! Ask me anything!", "ai", save=False)
+    paint_history()
+    message = st.chat_input("Ask a question about the document...")
+    if message:
+        send_message(message, "human")
+
+else:
+    st.session_state["messages"] = []
 
 
+
+
+        
